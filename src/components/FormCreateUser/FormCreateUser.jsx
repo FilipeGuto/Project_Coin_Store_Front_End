2598ec403e8498b3a36ac5ceb3dcd4191a5ec7a6
FilipeGuto <filipeguto95@gmail.com>
2022-05-13 16:10:48 -0300

    FEAT: Estilização da page de Login e Registro

--- src/components/FormCreateUser/FormCreateUser.jsx
+++ src/components/FormCreateUser/FormCreateUser.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
-import { createNewUser } from '../../services/users';
-import { useNavigate } from 'react-router-dom';
-import {
-  Form,
-  Button,
-} from "react-bootstrap";
-
+import { createNewUser } from "../../services/users";
+import { useNavigate } from "react-router-dom";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 export default function FormCreateUser() {
   const [email, setEmail] = useState("");
@@ -23,48 +19,57 @@ export default function FormCreateUser() {
       password,
     };
 
-    const newUser = createNewUser(userData)
-    
-    if(newUser) {
+    const newUser = createNewUser(userData);
+
+    if (newUser) {
       alert("Email cadrastrado com sucesso");
-      navigate('/login');
+      navigate("/login");
     }
   };
 
   return (
-    <div className="form-login">
-      <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Insira seu nome</Form.Label>
-          <Form.Control
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Insira seu email</Form.Label>
-          <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Insira sua senha</Form.Label>
-          <Form.Control
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button
-        variant="primary"
-        type="submit"
-        >
-          CRIAR
-        </Button>
-      </Form>
+    <div className="ctn container-fluid">
+      <Container className="mt-5">
+        <h1 className="title-login text-center">COIN STORE</h1>
+        <Row className="mt-5">
+          <Col
+            lg={5}
+            md={6}
+            sm={12}
+            className="p-5 m-auto rounded-lg form-login"
+          >
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Insira seu nome</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Insira seu email</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Insira sua senha</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                CRIAR
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { loginUser } from "../../services/users";
 import { useNavigate } from "react-router-dom";
-import {
-  Form,
-  Button,
-} from "react-bootstrap";
+import { Form, Button, Container, Col, Row } from "react-bootstrap";
+import "./formLogin.css";
 
 export default function FormLogin() {
   const [email, setEmail] = useState("");
@@ -39,39 +37,38 @@ export default function FormLogin() {
   };
 
   return (
-    <div className="form-login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Insira seu email</Form.Label>
-          <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Insira sua senha</Form.Label>
-          <Form.Control
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button
-        variant="primary"
-        type="submit"
-        
-        >
-          Entrar
-        </Button>
-        {' '}
-        <Button
-        type="button"
-        onClick={handleCreateAccount}
-        >
-          Ainda não tenho conta
-        </Button>
-      </Form>
+    <div className="ctn container-fluid">
+      <Container className="mt-5">
+        <h1 className="title-login text-center">COIN STORE</h1>
+        <Row className="mt-5">
+          <Col lg={5} md={6} sm={12} className="p-5 m-auto rounded-lg form-login">
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3">
+                <Form.Label>Insira seu email</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Insira sua senha</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Entrar
+              </Button>{" "}
+              <Button type="button" onClick={handleCreateAccount}>
+                Registrar
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
