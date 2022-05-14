afbdc6ecbf215c3b15ccbb7b115972428eba7d22
FilipeGuto <filipeguto95@gmail.com>
2022-05-13 21:09:50 -0300

    FEAT: Estilização de info dos users

--- src/components/InfoUsers/InfoUsers.jsx
+++ src/components/InfoUsers/InfoUsers.jsx
@@ -3,6 +3,7 @@ import { getAllUsers } from "../../services/users";
 import { useNavigate } from "react-router-dom";
 import Context from "../../Context/Context";
 import { Table } from "react-bootstrap";
+import "./infousers.css"
 
 export default function InfoUsers() {
   const [data, setData] = useState([]);
@@ -32,9 +33,10 @@ export default function InfoUsers() {
 
   return (
     <div>
-      <Table striped bordered>
+      <Table striped bordered >
         <thead>
           <tr>
+            <th>Id</th>
             <th>Nome</th>
             <th>Email</th>
             <th>Moedas</th>
@@ -44,15 +46,17 @@ export default function InfoUsers() {
           {data.map((user) => (
             user.role === "admin" ? null : (
             <tr>
+              <td>{user._id}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>{user.coin}</td>
-              <td
+              <button
                 type="button"
+                className="btn-users"
                 onClick={() => handleCoins(user.email, user.coin)}
               >
-                Alterar Moedas
-              </td>
+                Alterar moedas
+              </button>
             </tr>
             )
           ))}
